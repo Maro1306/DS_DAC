d442614a3914d128d610898ed51fd6647057d6b7
Maria <youshow25.today@gmail.com>
2016-03-14 21:52:45 +0900

    [add] かけ算モジュール追加

---
+++ MULS_FIXP_sim.cpp
@@ -0,0 +1,73 @@
+#include <stdio.h>
+#include <iostream>
+
+#include <verilated.h>
+#include "debug/obj_dir/VMULS_FIXP.h"
+
+#define PLUS	0
+#define MINUS	1
+
+using namespace std;
+
+VMULS_FIXP *mulf_i;
+
+unsigned long int pc;
+
+double sc_time_stamp(){
+	return pc;
+}
+
+void init(){
+	mulf_i->m_clock=0;
+	mulf_i->p_reset=0;
+	mulf_i->eval();
+	mulf_i->m_clock=1;
+	mulf_i->p_reset=1;
+	mulf_i->eval();
+	mulf_i->m_clock=0;
+	mulf_i->p_reset=0;
+	mulf_i->eval();
+	pc=0;
+}
+
+void falling_clock(){
+	mulf_i->m_clock=0;
+	mulf_i->p_reset=1;
+	mulf_i->eval();
+}
+
+void rising_clock(){
+	mulf_i->m_clock=1;
+	pc++;
+	mulf_i->eval();
+}
+
+void exe_mulf(int asin, int ain, int bsin, int bin){
+	falling_clock();
+
+	// input
+	mulf_i->as = asin;
+	mulf_i->a = ain;
+	mulf_i->bs = bsin;
+	mulf_i->b = bin;
+	mulf_i->exe = 1;
+	
+	rising_clock();
+
+	// output
+	printf("%d %d\n", mulf_i->rs, mulf_i->r);
+}
+
+int main(int argv,char *argc[]){
+	mulf_i = new VMULS_FIXP();
+
+	init();
+
+	exe_mulf(PLUS, 100, PLUS, 200);
+	exe_mulf(MINUS, 100, PLUS, 200);
+	exe_mulf(PLUS, 100, MINUS, 200);
+	exe_mulf(MINUS, 100, MINUS, 200);
+
+	return 0;
+}
+
